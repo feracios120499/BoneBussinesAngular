c439c2dff750e2e5af1e1da535e5ca6f8b663afa
Max <37274332+feracios120499@users.noreply.github.com>
2022-04-28 15:28:03 +0300

    add close modal

--- src/app/@core/services/modal.service.ts
+++ src/app/@core/services/modal.service.ts
@@ -1,33 +1,45 @@
 import { Injectable } from '@angular/core';
+import { UserEditModalComponent } from '@modules/users/components/user-edit-modal/user-edit-modal.component';
 import { NgbModal, NgbModalOptions, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class ModalService {
-    constructor(private modalService: NgbModal) {
+  private modals: NgbModalRef[] = [];
+  constructor(private modalService: NgbModal) {
+    this.modalService.activeInstances.asObservable().subscribe((modals) => {
+      this.modals = modals;
+    });
+  }
 
-    }
+  open(content: any, options?: NgbModalOptions | undefined): NgbModalRef {
+    if (!document.body.classList.contains('modal-open')) {
+      const offset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+      document.body.style.top = offset * -1 + 'px';
 
-    open(content: any, options?: NgbModalOptions | undefined): NgbModalRef {
-        if (!document.body.classList.contains('modal-open')) {
-            const offset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-            document.body.style.top = (offset * -1) + 'px';
-
-            const modalRef = this.modalService.open(content, options);
-            modalRef.result.then(
-                () => {
-                    window.scrollTo(0, offset);
-                    document.body.style.top = 'auto';
-                },
-                () => {
-                    window.scrollTo(0, offset);
-                    document.body.style.top = 'auto';
-                });
-
-
-            return modalRef;
+      const modalRef = this.modalService.open(content, options);
+      console.log(modalRef.componentInstance);
+      modalRef.result.then(
+        () => {
+          window.scrollTo(0, offset);
+          document.body.style.top = 'auto';
+        },
+        () => {
+          window.scrollTo(0, offset);
+          document.body.style.top = 'auto';
         }
-        return this.modalService.open(content, options);
+      );
+
+      return modalRef;
     }
+    return this.modalService.open(content, options);
+  }
+
+  close(content: any): void {
+    const modal = this.modals.find((p) => p.componentInstance instanceof content);
+    if (modal) {
+      modal.close();
+    }
+  }
 }
