3f7c1411b332ad06ae5086d0cbf9d59485302353
Petr <korolyov.petr.mich@gmail.com>
2022-05-02 14:57:01 +0300

    update correxpondents  loading

---
+++ src/app/modules/users/models/user-edit-modal-result.model.ts
@@ -0,0 +1,6 @@
+import { UserEditForm } from './UserEdit-form.model';
+import { UserEditUpdateModel } from './UserEdit-update.model';
+
+export type UserEditModalResult = UserEditUpdateModel | UserEditForm;
+{
+}
