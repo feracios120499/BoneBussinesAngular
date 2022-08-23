205b37faa9b550cfc042c4c812b8c87aab7b482e
Maksym Pishchuk <maksym.pishchuk@unity-bars.com>
2022-08-23 19:29:35 +0300

    payment modal completed

--- src/app/@shared/components/form-controls/b1-list-documents/b1-list-documents.component.ts
+++ src/app/@shared/components/form-controls/b1-list-documents/b1-list-documents.component.ts
@@ -4,14 +4,40 @@ import { PaymentSupDoc } from '@models/payments/payment-sup-doc.model';
 @Component({
   selector: 'b1-list-documents',
   templateUrl: './b1-list-documents.component.html',
-  styleUrls: ['./b1-list-documents.component.scss']
+  styleUrls: ['./b1-list-documents.component.scss'],
 })
 export class B1ListDocumentsComponent implements OnInit {
-  constructor() { }
+  constructor() {}
 
   @Input() documents?: PaymentSupDoc[];
 
+  currentDocument?: PaymentSupDoc;
+  showDocument?: boolean;
+
+  onSupdocumentSelect(doc: PaymentSupDoc): void {
+    if (this.currentDocument == doc) {
+      this.currentDocument = undefined;
+      this.showDocument = false;
+      return;
+    }
+    this.currentDocument = doc;
+    this.showDocument = true;
+    console.log(this.currentDocument.supDoc.fileName);
+  }
+
+  formatBytes(bytes: any, decimals = 2) {
+    if (bytes === 0) return '0 Bytes';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'kB', 'MB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  }
 
   ngOnInit(): void {
+    this.showDocument = false;
   }
 }
--- src/app/@shared/components/form-controls/b1-list-documents/b1-list-documents.module.ts
+++ src/app/@shared/components/form-controls/b1-list-documents/b1-list-documents.module.ts
@@ -5,10 +5,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { B1IconModule } from '@directives/b1-icon/b1-icon.module';
 
 import { B1ListDocumentsComponent } from './b1-list-documents.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { NativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [B1ListDocumentsComponent],
-  imports: [CommonModule, NgbModule, B1IconModule],
+  imports: [CommonModule, NgbModule, B1IconModule, TranslateModule, NativeDateModule],
   exports: [B1ListDocumentsComponent],
 })
 export class B1ListDocumentsModule {}
